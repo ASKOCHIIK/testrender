7ea9dcb826cf047b6ba377c8fadd6f299559459d
Askat Kulmanov <askatkulmanov1gmail.com@MacBook-Air-Askat.local>
2025-12-18 04:14:38 +0600

    fix ALLOWED_HOSTS for render

--- config/settings.py
+++ config/settings.py
@@ -23,11 +23,13 @@ environ.Env.read_env(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY", default="unsafe-secret-key-for-render")
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["*"])
+
 CSRF_TRUSTED_ORIGINS = [
     "https://bootcamp-37.onrender.com",
 ]
